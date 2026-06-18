8fb87703cb2bf3f7ec288de903ea4df2040c0432
JayLeet <jaygames1337@gmail.com>
2026-06-18 02:28:06 +0200

    Update systems/startup/gameLifecycleRuntime

--- systems/startup/gameLifecycleRuntime.js
+++ systems/startup/gameLifecycleRuntime.js
@@ -1,4 +1,4 @@
-const GameLifecycleManager = require('../game/GameLifecycleManager')
+const GameLifecycleManager = require('../GameLifecycleManager')
 const {
   registerRuntimeMaintenanceTask
 } = require('../../utils/runtimeMaintenance')
