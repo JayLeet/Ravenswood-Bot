0559f8918b8b27d1bb722e406da1f28880d20771
JayLeet <jaygames1337@gmail.com>
2026-06-18 02:28:07 +0200

    Update utils/botcChannelNames

--- utils/botcChannelNames.js
+++ utils/botcChannelNames.js
@@ -2,8 +2,13 @@ const AUTO_SETUP_CATEGORY_NAME = 'Ravenswood Bluff'
 const BOT_UPDATE_CHANNEL_NAME = '🤖-botc-bot-channel'
 const BOT_UPDATE_CHANNEL_SOURCE = 'botc-bot-channel'
 
+function normalizeChannelName(name) {
+  return String(name || '').toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-+|-+$/g, '')
+}
+
 module.exports = {
   AUTO_SETUP_CATEGORY_NAME,
   BOT_UPDATE_CHANNEL_NAME,
-  BOT_UPDATE_CHANNEL_SOURCE
+  BOT_UPDATE_CHANNEL_SOURCE,
+  normalizeChannelName
 }
