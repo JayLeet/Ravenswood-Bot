045a522c0a6fed579de69cb79223b0ddfe900dcd
JayLeet <jaygames1337@gmail.com>
2026-06-18 02:28:01 +0200

    Update systems/discord/interactions/storytellerDashboard/grimRevealButton

--- systems/discord/interactions/storytellerDashboard/grimRevealButton.js
+++ systems/discord/interactions/storytellerDashboard/grimRevealButton.js
@@ -4,14 +4,22 @@ const {
 } = require('../../embeds')
 const {
   acknowledgeInteraction,
+  createSystemEmbed,
   editDashboardFailure
 } = require('../feedback')
 const {
   cleanupSetupChannels
 } = require('../../../../utils/channelCleanup')
 const {
+  queuedChannelSend,
   queuedMessageEdit
 } = require('../../../../utils/discord/messageActions')
+const {
+  createEndGameLogComponents
+} = require('../../../../utils/gameLogEndResult')
+const {
+  getPostGameRevealChannel
+} = require('./revealChannel')
 const {
   createBotLogger
 } = require('../../../../utils/logger')
@@ -19,8 +27,10 @@ const {
 const log = createBotLogger({ subsystem: 'GrimRevealButton' })
 
 function createGrimRevealButtonHandler({
+  deletePendingGameSummary = null,
   gameLifecycle,
-  getDashboardPlayerLabels
+  getDashboardPlayerLabels,
+  serverConfigs = null
 }) {
   return async function handleGrimRevealButton(interaction, context) {
     const reveal = parseGrimRevealCustomId(interaction.customId)
@@ -47,12 +57,66 @@ function createGrimRevealButtonHandler({
         })
       })
 
-    if (result.cleanupSetupChannels) await cleanupSetupChannels(interaction.client, context.serverConfig)
+    if (result.cleanupSetupChannels) {
+      await cleanupSetupChannels(interaction.client, context.serverConfig)
+      await sendFinalGameLogNotice({
+        deletePendingGameSummary,
+        interaction,
+        result,
+        serverConfig: context.serverConfig,
+        serverConfigs
+      })
+    }
 
     return true
   }
 }
 
+async function sendFinalGameLogNotice({
+  deletePendingGameSummary,
+  interaction,
+  result,
+  serverConfig,
+  serverConfigs
+}) {
+  const components = await createEndGameLogComponents({
+    client: interaction.client,
+    deletePendingGameSummary,
+    guildId: interaction.guild.id,
+    result,
+    serverConfigs
+  })
+  if (!components.length) return null
+
+  const channel = await getPostGameRevealChannel(interaction.client, serverConfig)
+  if (!isConfiguredPostGameChannel(channel, serverConfig)) return null
+  if (!channel?.isTextBased?.()) return null
+
+  return queuedChannelSend(channel, {
+    embeds: [
+      createSystemEmbed(
+        'Game history ready',
+        'The game is fully revealed. Save or discard this game history here.',
+        0x3498db
+      )
+    ],
+    components
+  }).catch(err => {
+    log.recoverable('send-final-game-log-notice', err, {
+      channelId: channel.id,
+      guildId: interaction.guild?.id,
+      summaryId: result.pendingSummary?.id
+    })
+    return null
+  })
+}
+
+function isConfiguredPostGameChannel(channel, serverConfig) {
+  return Boolean(channel?.id && serverConfig?.postGameChannelId && channel.id === serverConfig.postGameChannelId)
+}
+
 module.exports = {
-  createGrimRevealButtonHandler
+  isConfiguredPostGameChannel,
+  createGrimRevealButtonHandler,
+  sendFinalGameLogNotice
 }
