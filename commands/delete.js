2c03c03e204388d31a3105b96950b7ef561c1fab
JayLeet <jaygames1337@gmail.com>
2026-06-18 02:27:51 +0200

    Update commands/delete

--- commands/delete.js
+++ commands/delete.js
@@ -1,16 +1,16 @@
 const { PermissionFlagsBits } = require('discord.js')
-const { wrapCommand } = require('../systems/discord/interactions/commandWrapper')
+const { wrapCommand } = require('../utils/commandWrapper')
 const {
   executeSetupDelete
 } = require('../utils/setupDelete')
 
 module.exports = {
   name: 'delete',
-  description: 'Delete BOTC Bot setup channels and categories.',
+  description: 'Delete BOTC-managed setup channels and categories.',
   options: [],
   data: {
     name: 'delete',
-    description: 'Delete BOTC Bot setup channels and categories.',
+    description: 'Delete BOTC-managed setup channels and categories.',
     options: [],
     default_member_permissions: PermissionFlagsBits.Administrator.toString()
   },
@@ -19,5 +19,3 @@ module.exports = {
 
   execute: wrapCommand(executeSetupDelete)
 }
-
-module.exports.executeSetupDeleteCommand = executeSetupDelete
